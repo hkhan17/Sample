dfd56e253e4db9c446da7cd080fac73eccc66cdb
Hussain Khan <51316279+hkhan17@users.noreply.github.com>
2022-10-18 16:27:43 +0530

    Update doit.m

--- doit.m
+++ doit.m
@@ -5,7 +5,7 @@ import matlab.unittest.plugins.codecoverage.CoberturaFormat
 suite = testsuite('tDemo_myAdd.m');
 
 runner = TestRunner.withTextOutput;
-reportFormat = CoberturaFormat("cobertura.xml");
+reportFormat = CoberturaFormat("cobertura_custom.xml");
 runner.addPlugin(GeneratedCodeCoveragePlugin(Producing=reportFormat));
 
 runner.run(suite);
